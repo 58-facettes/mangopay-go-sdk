95d3627fbbb1b2a391897a42a413cf8036dce5e5
Henri Lepic <henrilepic@gmail.com>
2020-02-20 11:43:35 +0100

    fix: example test for getting a User

--- example_test.go
+++ example_test.go
@@ -4,5 +4,5 @@ import "github.com/58-facettes/mangopay-go-sdk"
 
 func ExampleNewWithBasicAuth() {
 	api := mangopay.NewWithBasicAuth("client-id", "client-password")
-	api.Users.ViewUser("1234")
+	api.Users.View("1234")
 }
